bfa2beb7bd2f462ef78dd3bc322a548cd99fc370
Gemma Campbell <gem.cam89@outlook.com>
2024-10-25 20:00:34 +0100

    Replaced RigidBody with Character Controller component

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Movement Variables
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    private Rigidbody rb;
+    public float moveSpeed;
+    public float jumpHeight;
+    public CharacterController controller;
 
     //Gravity variables
     Vector3 velocity;
@@ -18,32 +18,52 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
-    private bool isGrounded;
+    public bool isGrounded;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);  //Checks to see if collision is detected
+        GroundCheck();
+        Move();
 
-        if(isGrounded && velocity.y < 0 )
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = -2f;   //Removes excess velocity
-
+            velocity.y = jumpHeight;
         }
 
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+
+    void Move()
+    {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-         Vector3 move = transform.right * x + transform.forward * z;  //Allows player to move in all directions
+        Vector3 move = transform.right * x + transform.forward * z;  //Allows player to move in all directions
+        controller.Move(move * moveSpeed * Time.deltaTime);
 
-        transform.position += move * moveSpeed * Time.deltaTime;
+    }
 
+
+    void GroundCheck()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);  //Checks to see if collision is detected
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;   //Removes excess velocity
+        }
     }
 }
